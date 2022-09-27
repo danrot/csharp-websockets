53ac1a7727fc501c2bbfe7f31430d365570dfb46
Daniel Rotter <daniel.rotter@gmail.com>
2022-09-27 11:09:03 +0200

    Replace registering event listener with addEventListener

--- wwwroot/js/main.js
+++ wwwroot/js/main.js
@@ -4,14 +4,14 @@ const $updateButton = document.getElementById('update-button');
 
 const webSocket = new WebSocket(`wss://${location.host}/WebSocket`);
 
-webSocket.onopen = function (event) {
+webSocket.addEventListener('open', function(event) {
     $updateButton.addEventListener('click', function() {
         webSocket.send(JSON.stringify({type: 'update'}));
     });
-}
+});
 
-webSocket.onmessage = function (event) {
+webSocket.addEventListener('message', function(event) {
     const data = JSON.parse(event.data);
     $randomNumber.innerHTML = data.random;
     $creationDate.innerHTML = data.created;
-}
+});
